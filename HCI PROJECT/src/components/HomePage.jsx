083394de75a6a4b77cbaa9246e87ba6888ac1d65
peakyblinder101 <148172396+peakyblinder101@users.noreply.github.com>
2024-11-03 17:51:22 +0800

    Update HomePage.jsx

--- HCI PROJECT/src/components/HomePage.jsx
+++ HCI PROJECT/src/components/HomePage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import '../styles/HomePage.css'; // Add your own styles
+import '../styles/HomePage.css';
 
 const HomePage = () => {
     return (
         <div className="home-page">
             <h1>Welcome to BarcodeHealth!</h1>
             <p>Your journey towards healthier food choices begins here.</p>
-            {/* Add more content here as needed */}
         </div>
     );
 };
